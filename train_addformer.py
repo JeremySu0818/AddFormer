d540d81d6154545cba10defbe474e95e8c5920bd
JeremySu0818 <xinghong.su0818@gmail.com>
2026-02-04 14:17:48 +0800

    chore(config): Update default output directory name

--- train_addformer.py
+++ train_addformer.py
@@ -19,7 +19,7 @@ hf_logging.set_verbosity_error()
 ds_logging.set_verbosity_error()
 
 DATA_DIR = "data_add"
-OUTPUT_DIR = "addformer_result"
+OUTPUT_DIR = "addformer"
 MODEL_DIM = 64
 N_LAYER = 8
 N_HEAD = 8
